b01c2a51a0c5fbe3d91c6849bcd5356895b15655
Loutimi <Loutimi59@gmail.com>
2024-07-07 18:54:32 +0100

    Modify fns_and_dsa directory

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,4 +1,5 @@
 def display_menu():
+    print()
     print("Shopping List Manager")
     print("1. Add Item")
     print("2. Remove Item")
@@ -10,30 +11,30 @@ def main():
 
     while True:
         display_menu()
-        choice = int(input("Enter your choice: "))
+        choice = input("Enter your choice: ")
 
-        if choice == 1:
-            item = input("Enter item to add: ")
-            shopping_list.append(item)
-            print(f"'{item}' has been added to your shopping list.")
-        elif choice == 2:
+        if choice == '1':
+            add_item = input("Enter the item to add: ").strip().lower()
+            shopping_list.append(add_item)
+            print(f"'{add_item}' has been added to your shopping list.")
+        elif choice == '2':
             if len(shopping_list) == 0:
                 print("Your shopping list is empty.")
             else:
-                item = input("Enter item to remove: ")
-                if item in shopping_list:
-                    shopping_list.remove(item)
-                    print(f"'{item}' has been removed from your shopping list.")
+                remove_item = input("Enter the item to remove: ")
+                if remove_item in shopping_list:
+                    shopping_list.remove(remove_item)
+                    print(f"'{remove_item}' has been removed from your shopping list.")
                 else:
-                    print(f"'{item}' is not in your shopping list.")
-        elif choice == 3:
+                    print(f"'{remove_item}' is not in your shopping list.")
+        elif choice == '3':
             if len(shopping_list) == 0:
                 print("Your shopping list is empty.")
             else:
                 print("Your shopping list:")
                 for item in shopping_list:
                     print(item)
-        elif choice == 4:
+        elif choice == '4':
             print("Goodbye!")
             break
         else:
