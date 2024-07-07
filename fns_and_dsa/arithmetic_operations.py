305eea22bf221fb2a57f94c63862bfc161750889
Loutimi <Loutimi59@gmail.com>
2024-07-07 18:09:20 +0100

    Modify fns_and_dsa directory

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,16 +1,19 @@
-def perform_operation(num1:float,num2:float,operation:str):
-    match operation:
-        case "add" :
-            print(num1 + num2)   
-        case "subtract" :  
-            print(num1 - num2)
-        case "multiply" :
-            print(num1 * num2)
-        case "divide" :
-            if num1 and num2 > 0:
-                print(num1 / num2)
-            else:
-                print("Cannot divide by zero.")
-        case _ :
-            print("Invalid operator")
+def perform_operation(num1, num2, operation):
+    if operation == "add":
+        print(num1 + num2)   
+    
+    elif operation == "subtract":  
+        print(num1 - num2)
+    
+    elif operation == "multiply":
+        print(num1 * num2)
+    
+    elif operation == "divide" :
+        if num1 and num2 > 0:
+            print(num1 / num2)
+        else:
+            print("Cannot divide by zero.")
+    else:
+        print("Invalid operator")
     return
+perform_operation(1,3,"add")
